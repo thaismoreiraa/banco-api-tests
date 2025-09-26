7d0619c9dfe40c871b5b8a1a3bf3afa1e2f7219e
Thais Moreira <thais.dfmoreira@gmail.com>
2025-09-26 11:50:04 -0300

    Adicionando a biblioteca .env para variáveis globais

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest')
 const { expect } = require('chai')
+require('dotenv').config()
 
 describe('Transferências', () => {
   describe('POST /transferencias', () => {
     it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
-      const responseLogin = await request('http://localhost:3000')
+      const responseLogin = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
         .send({
@@ -15,7 +16,7 @@ describe('Transferências', () => {
       const token = responseLogin.body.token
 
 
-      const response = await request('http://localhost:3000')
+      const response = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
@@ -30,7 +31,7 @@ describe('Transferências', () => {
     
 
     it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$ 10,00 ', async () => {
-      const responseLogin = await request('http://localhost:3000')
+      const responseLogin = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
         .send({
@@ -41,7 +42,7 @@ describe('Transferências', () => {
       const token = responseLogin.body.token
 
 
-      const response = await request('http://localhost:3000')
+      const response = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)

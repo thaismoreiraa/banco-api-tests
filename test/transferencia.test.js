dc16ba780623d3bad57c66e6ec868303e979af72
Thais Moreira <thais.dfmoreira@gmail.com>
2025-09-27 13:56:05 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require('supertest')
 const { expect } = require('chai')
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferências', () => {
   describe('POST /transferencias', () => {
@@ -12,31 +13,26 @@ describe('Transferências', () => {
     })
 
     it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
+      const bodyTransferencias = { ...postTransferencias }
+
       const response = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
-        .send({
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 11,
-          token: ""
-        })
+        .send(bodyTransferencias)
       expect(response.status).to.equal(201)
     })
 
 
     it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$ 10,00 ', async () => {
+      const bodyTransferencias = { ...postTransferencias }
+      bodyTransferencias.valor = 7
+
       const response = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
-        .send({
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 7,
-          token: ""
-        })
+        .send(bodyTransferencias)
       expect(response.status).to.equal(422)
     })
   })

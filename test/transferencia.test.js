35a5ae674127f35626efafe09468b3a4e9f1b4c8
Thais Moreira <thais.dfmoreira@gmail.com>
2025-09-27 15:57:49 -0300

    Adicionei testes para GET transferências total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,12 +5,13 @@ const { obterToken } = require('../helpers/autenticacao')
 const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferências', () => {
-  describe('POST /transferencias', () => {
-    let token
+  let token
 
-    beforeEach(async () => {
-      token = await obterToken('julio.lima', '123456')
-    })
+  beforeEach(async () => {
+    token = await obterToken('julio.lima', '123456')
+  })
+
+  describe('POST /transferencias', () => {
 
     it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
       const bodyTransferencias = { ...postTransferencias }
@@ -34,6 +35,33 @@ describe('Transferências', () => {
         .set('Authorization', `Bearer ${token}`)
         .send(bodyTransferencias)
       expect(response.status).to.equal(422)
+    })
+  })
+
+  describe('GET /transferencias/{id}', () => {
+    it('Deve retornar sucesso com 200 e dados iguais ao registro de transferência contido no banco de dados quando o ID for válido ', async () => {
+      const response = await request(process.env.BASE_URL)
+        .get('/transferencias/2')
+        .set('Authorization', `Bearer ${token}`)
+
+      expect(response.status).to.equal(200)
+      expect(response.body.id).to.equal(2)
+      expect(response.body.id).to.be.a('number')
+      expect(response.body.conta_origem_id).to.equal(1)
+      expect(response.body.valor).to.equal(600)
+    })
+  })
+
+  describe('GET /transferencias', () => {
+    it('Deve retornar 10 elementos na paginação quando informar limite de 10 registros', async () => {
+      const response = await request(process.env.BASE_URL)
+        .get('/transferencias?page=1&limit=10')
+        .set('Authorization', `Bearer ${token}`)
+
+      expect(response.status).to.equal(200)  
+      expect(response.body.limit).to.equal(10)
+      expect(response.body.transferencias).to.have.lengthOf(10)
+
     })
   })
 })
